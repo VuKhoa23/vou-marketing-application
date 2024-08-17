284536355f31856f9d8576fabde7811d7c4e5d1b
huutamisme <hohuutam287@gmail.com>
2024-08-17 16:16:37 +0700

    add event banner file

---
+++ frontend/vendor-web/src/eventBanner.js
@@ -0,0 +1,3 @@
+export const EVENT_BANNER = [
+    { src: "EventBanner/Social Seoul Academy.jpg", alt: "123phim" },
+]
--- frontend/vendor-web/src/pages/Home/index.js
+++ frontend/vendor-web/src/pages/Home/index.js
@@ -2,12 +2,13 @@ import { DUMMY_GAMES } from '../../dummy-games.js';
 import Game from './components/Game.jsx';
 import Carousel from './components/Carousel.jsx';
 import { BRANDS_LOGO } from '../../brands-logo.js';
+import { EVENT_BANNER } from '../../eventBanner.js';
 
 function HomePage() {
     return (
         <>
             <div className='p-10'>
-                <Carousel images={BRANDS_LOGO} slidesToShow={4} slidesToScroll={4} dots={false} />
+                <Carousel images={EVENT_BANNER} slidesToShow={1} slidesToScroll={1} dots={false} />
             </div>
             <h1 className='text-3xl font-bold flex justify-center items-center m-10'>Các loại sự kiện</h1>
             <ul className='grid grid-cols-1 md:grid-cols-2 gap-4 m-10'>
