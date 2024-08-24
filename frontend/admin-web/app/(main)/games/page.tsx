ce6b7bf96a3fdfdaded9a8415a221dea02efdab3
qhuongng <nq.huong.2304@gmail.com>
2024-08-24 21:01:49 +0700

    Remove add and delete modals; Refine edit form fields & add form actions

--- frontend/admin-web/app/(main)/games/page.tsx
+++ frontend/admin-web/app/(main)/games/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { createColumnHelper } from "@tanstack/react-table";
 import { useFormik } from "formik";
 import * as yup from "yup";
@@ -8,6 +8,10 @@ import Table from "@/components/Table";
 import { Game } from "@/lib/types/Game";
 import gameData from "@/lib/mock_data/games.json";
 import Modal from "@/components/Modal";
+
+// duplicate data & get current max ID to generate new iD when adding users
+// TODO: REMOVE WHEN CONNECTED TO BACKEND)
+let data = [...gameData];
 
 // add form validation schema
 export const addSchema = yup.object().shape({
@@ -24,14 +28,25 @@ export const editSchema = yup.object().shape({
 });
 
 export default function Games() {
-    const [addOpen, setAddOpen] = useState(false);
-    const handleToggleAdd = () => setAddOpen((prev) => !prev);
+    const [editingGame, setEditingGame] = useState<Game>();
 
     const [editOpen, setEditOpen] = useState(false);
     const handleToggleEdit = () => setEditOpen((prev) => !prev);
 
-    const [deleteOpen, setDeleteOpen] = useState(false);
-    const handleToggleDelete = () => setDeleteOpen((prev) => !prev);
+    const [tableData, setTableData] = useState(data);
+
+    // update the edit form whenever the selected game changes
+    useEffect(() => {
+        if (editingGame) {
+            editFormik.setValues({
+                id: editingGame.id,
+                name: editingGame.name,
+                type: editingGame.type,
+                allowsTrade: editingGame.allowsTrade,
+                instructions: editingGame.instructions,
+            });
+        }
+    }, [editingGame]);
 
     // define columns for user table
     const columnHelper = createColumnHelper<Game>();
@@ -52,81 +67,77 @@ export default function Games() {
             cell: (info) => <span>{info.getValue()}</span>,
             header: () => <span>Loại trò chơi</span>,
         }),
+        columnHelper.accessor((row) => row.allowsTrade, {
+            id: "allowsTrade",
+            cell: (info) => (
+                <input
+                    type="checkbox"
+                    className="checkbox checkbox-sm"
+                    disabled
+                    checked={info.getValue()}
+                />
+            ),
+            header: () => <span>Đổi vật phẩm?</span>,
+        }),
         columnHelper.accessor((row) => row.instructions, {
             id: "instructions",
             cell: (info) => <span>{info.getValue()}</span>,
             header: () => <span>Hướng dẫn chơi</span>,
         }),
-        columnHelper.accessor((row) => row.id, {
+        columnHelper.accessor((row) => row, {
             id: "edit-del",
             cell: (info) => (
-                <div className="flex justify-between">
-                    {/* edit button */}
-                    <button className="btn btn-square btn-sm" onClick={handleToggleEdit}>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="size-5"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
-                            />
-                        </svg>
-                    </button>
-
-                    {/* delete button */}
-                    <button
-                        className="btn btn-error btn-square btn-sm"
-                        onClick={handleToggleDelete}
+                <button
+                    className="btn btn-square btn-sm"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        setEditingGame(info.getValue());
+                        handleToggleEdit();
+                    }}
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="size-5"
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="size-5"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                            />
-                        </svg>
-                    </button>
-                </div>
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                        />
+                    </svg>
+                </button>
             ),
             header: () => <span></span>,
         }),
     ];
 
-    // add form
-    const addFormik = useFormik({
-        initialValues: {
-            name: "",
-            type: "",
-            instructions: "",
-        },
-        validationSchema: addSchema,
-        onSubmit: async (values) => {
-            handleToggleAdd();
-        },
-    });
-
     // edit form
     const editFormik = useFormik({
         initialValues: {
-            name: "",
-            type: "",
-            instructions: "",
+            id: editingGame?.id,
+            name: editingGame?.name,
+            type: editingGame?.type,
+            allowsTrade: editingGame?.allowsTrade,
+            instructions: editingGame?.instructions,
         },
         validationSchema: editSchema,
         onSubmit: async (values) => {
+            const newGame: Game = {
+                id: values.id!,
+                name: values.name!,
+                type: values.type!,
+                allowsTrade: values.allowsTrade!,
+                instructions: values.instructions!,
+            };
+
+            data = data.map((g) => (g.id === newGame.id ? { ...newGame } : g));
+
+            setTableData(data);
+
             handleToggleEdit();
         },
     });
@@ -135,111 +146,14 @@ export default function Games() {
         <div className="flex-col">
             <h1>Quản lý trò chơi</h1>
 
-            <div className="flex justify-end mb-4">
-                <button className="btn btn-primary" onClick={handleToggleAdd}>
-                    Thêm trò chơi
-                </button>
-            </div>
-
             <div className="card bg-base-100 shadow w-full overflow-x-auto">
                 <div className="card-body">
-                    <Table columnDef={GameColumns} data={gameData} />
+                    <Table columnDef={GameColumns} data={tableData} />
                 </div>
             </div>
 
-            {/* add modal */}
-            <Modal
-                open={addOpen}
-                onClose={() => {
-                    addFormik.resetForm();
-                }}
-            >
-                <h3 className="font-bold text-lg">Thêm trò chơi</h3>
-                <form
-                    method="dialog"
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                    }}
-                >
-                    <div>
-                        <label className="form-control w-full max-w-xs">
-                            <div className="label">
-                                <span className="label-text">Tên trò chơi</span>
-                            </div>
-                            <input
-                                name="name"
-                                type="text"
-                                placeholder="Tên trò chơi"
-                                className="input input-bordered w-full max-w-xs"
-                                value={addFormik.values.name}
-                                onChange={addFormik.handleChange}
-                            />
-                            {addFormik.touched.name && addFormik.errors.name && (
-                                <div className="form-error-msg">{addFormik.errors.name}</div>
-                            )}
-                        </label>
-
-                        <label className="form-control w-full max-w-xs">
-                            <div className="label">
-                                <span className="label-text">Loại trò chơi</span>
-                            </div>
-                            <input
-                                name="type"
-                                type="text"
-                                placeholder="Loại trò chơi"
-                                className="input input-bordered w-full max-w-xs"
-                                value={addFormik.values.type}
-                                onChange={addFormik.handleChange}
-                            />
-                            {addFormik.touched.type && addFormik.errors.type && (
-                                <div className="form-error-msg">{addFormik.errors.type}</div>
-                            )}
-                        </label>
-
-                        <label className="form-control w-full max-w-xs">
-                            <div className="label">
-                                <span className="label-text">Hướng dẫn chơi</span>
-                            </div>
-                            <textarea
-                                name="instructions"
-                                placeholder="Hướng dẫn chơi"
-                                className="textarea textarea-bordered textarea-md w-full max-w-xs"
-                                value={addFormik.values.instructions}
-                                onChange={addFormik.handleChange}
-                            />
-                            {addFormik.touched.instructions && addFormik.errors.instructions && (
-                                <div className="form-error-msg">
-                                    {addFormik.errors.instructions}
-                                </div>
-                            )}
-                        </label>
-                    </div>
-
-                    <div className="modal-action">
-                        <label className="btn btn-secondary" onClick={handleToggleAdd}>
-                            Hủy
-                        </label>
-                        <button
-                            type="submit"
-                            onClick={(e) => {
-                                e.preventDefault();
-                                addFormik.handleSubmit();
-                            }}
-                            className="btn btn-primary"
-                        >
-                            Thêm
-                        </button>
-                    </div>
-                </form>
-            </Modal>
-
             {/* edit modal */}
-            <Modal
-                open={editOpen}
-                onClose={() => {
-                    editFormik.resetForm();
-                }}
-            >
+            <Modal open={editOpen}>
                 <h3 className="font-bold text-lg">Cập nhật thông tin trò chơi</h3>
                 <form
                     method="dialog"
@@ -248,15 +162,28 @@ export default function Games() {
                     }}
                 >
                     <div>
-                        <label className="form-control w-full max-w-xs">
+                        <label className="form-control w-full mt-3">
                             <div className="label">
-                                <span className="label-text">Tên trò chơi</span>
+                                <span className="label-text font-bold">ID</span>
+                            </div>
+                            <input
+                                name="id"
+                                type="text"
+                                className="input input-bordered w-full"
+                                value={editFormik.values.id}
+                                disabled
+                            />
+                        </label>
+
+                        <label className="form-control w-full mt-3">
+                            <div className="label">
+                                <span className="label-text font-bold">Tên trò chơi</span>
                             </div>
                             <input
                                 name="name"
                                 type="text"
                                 placeholder="Tên trò chơi"
-                                className="input input-bordered w-full max-w-xs"
+                                className="input input-bordered w-full"
                                 value={editFormik.values.name}
                                 onChange={editFormik.handleChange}
                             />
@@ -265,15 +192,15 @@ export default function Games() {
                             )}
                         </label>
 
-                        <label className="form-control w-full max-w-xs">
+                        <label className="form-control w-full mt-3">
                             <div className="label">
-                                <span className="label-text">Loại trò chơi</span>
+                                <span className="label-text font-bold">Loại trò chơi</span>
                             </div>
                             <input
                                 name="type"
                                 type="text"
                                 placeholder="Loại trò chơi"
-                                className="input input-bordered w-full max-w-xs"
+                                className="input input-bordered w-full"
                                 value={editFormik.values.type}
                                 onChange={editFormik.handleChange}
                             />
@@ -282,16 +209,31 @@ export default function Games() {
                             )}
                         </label>
 
-                        <label className="form-control w-full max-w-xs">
+                        <div className="form-control w-full mt-3">
+                            <label className="label cursor-pointer">
+                                <span className="label-text font-bold">
+                                    Cho phép người chơi trao đổi vật phẩm
+                                </span>
+                                <input
+                                    type="checkbox"
+                                    name="allowsTrade"
+                                    onChange={editFormik.handleChange}
+                                    checked={editFormik.values.allowsTrade}
+                                    className="checkbox"
+                                />
+                            </label>
+                        </div>
+
+                        <label className="form-control w-full mt-3">
                             <div className="label">
-                                <span className="label-text">Hướng dẫn chơi</span>
+                                <span className="label-text font-bold">Hướng dẫn chơi</span>
                             </div>
                             <textarea
                                 name="instructions"
                                 placeholder="Hướng dẫn chơi"
-                                className="textarea textarea-bordered textarea-md w-full max-w-xs"
-                                value={addFormik.values.instructions}
-                                onChange={addFormik.handleChange}
+                                className="textarea textarea-bordered textarea-md w-full"
+                                value={editFormik.values.instructions}
+                                onChange={editFormik.handleChange}
                             />
                             {editFormik.touched.instructions && editFormik.errors.instructions && (
                                 <div className="form-error-msg">
@@ -317,20 +259,6 @@ export default function Games() {
                         </button>
                     </div>
                 </form>
-            </Modal>
-
-            {/* delete modal */}
-            <Modal open={deleteOpen}>
-                <h3 className="font-bold text-lg">Xóa trò chơi</h3>
-                <p className="py-4">Bạn có chắc chắn muốn xóa trò chơi này?</p>
-                <div className="modal-action">
-                    <label className="btn btn-secondary" onClick={handleToggleDelete}>
-                        Hủy
-                    </label>
-                    <label className="btn btn-error" onClick={handleToggleDelete}>
-                        Xóa
-                    </label>
-                </div>
             </Modal>
         </div>
     );
