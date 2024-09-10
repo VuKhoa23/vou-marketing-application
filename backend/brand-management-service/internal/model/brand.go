7df0d87ec5990212c1115cf8d9f3ea657946040b
Tricker14 <trietthetricker@gmail.com>
2024-09-10 16:24:34 +0700

    update brand attributes

--- backend/brand-management-service/internal/model/brand.go
+++ backend/brand-management-service/internal/model/brand.go
@@ -5,5 +5,8 @@ type Brand struct {
 	Username string `json:"username,omitempty"`
 	Password string `json:"password,omitempty"`
 	Category string `json:"category,omitempty"`
+	Address  string `json:"address,omitempty"`
+	Long     string `json:"long,omitempty"`
+	Lat      string `json:"lat,omitempty"`
 	State    bool   `json:"state"`
 }
