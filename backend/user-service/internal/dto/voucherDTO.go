f562a1b83579cd3f34e0f4b484e0a74e35fced2b
Tricker14 <trietthetricker@gmail.com>
2024-09-11 19:39:25 +0700

    update voucher

--- backend/user-service/internal/dto/voucherDTO.go
+++ backend/user-service/internal/dto/voucherDTO.go
@@ -6,6 +6,7 @@ type VoucherDTO struct {
 	VoucherValue       float64 `json:"voucherValue"`
 	VoucherDescription string  `json:"voucherDescription"`
 	VoucherQuantities  int     `json:"voucherQuantities"`
+	VoucherLeft        int     `json:"voucherLeft"`
 	VoucherEndDate     int64   `json:"voucherEndDate"`
 	VoucherStatus      bool    `json:"voucherStatus"`
 }
